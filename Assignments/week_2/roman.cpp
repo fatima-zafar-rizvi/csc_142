9a0ea4ef6beca677dbe4292d0e1b4859c2d7a679
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2025-02-12 11:48:21 -0500

    Add files via upload

--- Assignments/week_2/roman.cpp
+++ Assignments/week_2/roman.cpp
@@ -18,7 +18,7 @@ int main() {
 
     //Prompt user until a valid number is entered:
     do {
-        std::cout << "Enter an integer between 1 and 10: ";
+        std::cout << "Enter an integer between 1 and 100: ";
         std::cin >> number;
 
         if (number < 1) {
@@ -27,9 +27,9 @@ int main() {
             << std::endl;
         } 
         else {
-            if (number > 10) {
+            if (number > 100) {
                 std::cout 
-                << "Invalid input. Please enter a number of at most 10." 
+                << "Invalid input. Please enter a number of at most 100." 
                 << std::endl;
             } 
             else {
@@ -41,16 +41,32 @@ int main() {
     // Display the corresponding Roman numeral:
     std::cout << "The Roman numeral equvalent of " << number << " is: ";
 
-    if (number == 1) std::cout << "I";
-    else if (number == 2) std::cout << "II";
-    else if (number == 3) std::cout << "III";
-    else if (number == 4) std::cout << "IV";
-    else if (number == 5) std::cout << "V";
-    else if (number == 6) std::cout << "VI";
-    else if (number == 7) std::cout << "VII";
-    else if (number == 8) std::cout << "VIII";
-    else if (number == 9) std::cout << "IX";
-    else if (number == 10) std::cout << "X";
+    // Determine tens and ones place:
+    int tens = number /10;
+    int ones = number % 10;
+
+    // Convert tens place:
+    if (tens == 1) std::cout << "X";
+    else if (tens == 2) std::cout << "XX";
+    else if (tens == 3) std::cout << "XXX";
+    else if (tens == 4) std::cout << "XL";
+    else if (tens == 5) std::cout << "L";
+    else if (tens == 6) std::cout << "LX";
+    else if (tens == 7) std::cout << "LXX";
+    else if (tens == 8) std::cout << "LXXX";
+    else if (tens == 9) std::cout << "XC";
+    else if (tens == 10) std::cout << "C";
+
+    // Convert ones place:
+    if (ones == 1) std::cout << "I";
+    else if (ones == 2) std::cout << "II";
+    else if (ones == 3) std::cout << "III";
+    else if (ones == 4) std::cout << "IV";
+    else if (ones == 5) std::cout << "V";
+    else if (ones == 6) std::cout << "VI";
+    else if (ones == 7) std::cout << "VII";
+    else if (ones == 8) std::cout << "VIII";
+    else if (ones == 9) std::cout << "IX";
 
     std::cout  << std::endl;
 
