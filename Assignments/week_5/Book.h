8c22a9ee411a268ba28dfec0d890f1a51723a7cb
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2025-02-21 21:49:57 -0500

    Add files via upload

---
+++ Assignments/week_5/Book.h
@@ -0,0 +1,32 @@
+// Book.h
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <string>
+
+class Book {
+    private:
+        std::string codeNum;
+        std::string author;
+        std::string title;
+        int edition;
+        std::string pubCode;
+        double price;
+    
+    public:
+        // Constructor
+        Book(std::string cn, std::string au, std::string ti, int ed, std::string pc, double pr);
+    
+        // Accessor Methods
+        std::string getTitle() const;
+        std::string getAuthor() const;
+        double getPrice() const;
+        int getEdition() const;
+        std::string getISBN() const;
+        std::string getPubCode() const;
+    
+        // Mutator Method
+        void changePrice(double newPrice);
+    };
+    
+    #endif
