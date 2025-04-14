2fdc8ee09dc98ccb06c2dc6e0a4b1f5ead25a5b9
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2025-04-14 12:11:32 -0400

    Add files via upload

---
+++ Assignments/week_12/8-1_FibonacciRange.h
@@ -0,0 +1,25 @@
+// FibonacciRange.h
+#ifndef FIBONACCIRANGE_H
+#define FIBONACCIRANGE_H
+
+#include "FibonacciIterator.h"
+
+struct FibonacciRange {
+
+	explicit FibonacciRange(int max) : max{ max } { }
+
+	FibonacciIterator begin() const {
+
+		return FibonacciIterator{};
+	}
+
+	int end() const {
+
+		return max;
+	}
+
+private:
+	const int max;
+};
+
+#endif 
