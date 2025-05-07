7c9f176f4f67e1bb6d0d0148ba30e3beb510f2b6
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2025-05-06 22:01:38 -0400

    Update Easy_Assignment.cpp

--- Assignments/Easy_Assignment.cpp
+++ Assignments/Easy_Assignment.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
-void countdown_to_lightspeed(int seconds) {
-    for (int i = seconds; i > 0; --i) {
-        std::cout << "Jumping in " << i << "...\n";
-    }
-    std::cout << "Punch it, Chewie!\n";
-}
+int main(){
 
-int main() {
-    countdown_to_lightspeed(5);
+    std::cout << "Countdown starts now!\n";
+
+    for (int i = 10; i >=1; --i){
+        std::cout << i << "...\n";
+    }
+
+    std::cout << "Liftoff!\n";
+
     return 0;
 }
